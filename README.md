b0a89407abadd97217d8e2598dc5c3493248f84c
Alexander Dickbauer <alexander.dickbauer@nowconsultians.at>
2022-01-14 01:00:58 +0100

    Added create instructionset function and edited readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Busy_Beaver
+
+## Idea
+### Setup
+The tape is initialzied  with a size of 2048 and every Touring Machine uses the same tape.
+
+### Crossover
+The general idea is to generate 10bit long instruction sets.<br>
+These instruction sets can be easily crossovered by swapping segments between two parents <br>
+
+### Mutation
+After the crossover there is a chance that the child mutates one random bit in the instruction set.
+If no improvement is seen in the result the chance is increased up to 100% and it resets to the default value if the average of the elite improves.
+
+### Validation
+Because we have 13 States a validator is needed. The following things have to be checked:
+
+* are the last 4 bits <= 1100 (12)
+* is the first bit a 0
+* is the 3 bit a 1
+* atleast one instruction in the set has to contain 1100 (12) as the next step 
+
+If one of the results is negative we can skip the touring path and let the child die.<br>
+On the other hand, if the validator checks are positive the touring path beginns.
+
+#### Example
+
+    // crossover
+    parent_1 = 0b0111000100
+    parent_2 = 0b0101110110
+    
+    child_1 = 0b0111110110
+    child_2 = 0b0101000100
+    
+    //Mutation
+    child_2 = 0b0111000100
+
+
+### The Touring Path
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,49 @@
 #include <iostream>
 #include <vector>
 
-std::vector<int> tape(7);
+std::random_device r;
+std::default_random_engine rand_eng(r());
+std::uniform_int_distribution<int> card_selector(0, 3);
+std::uniform_int_distribution<int> state_selector(0, 12);
 
-struct Card
-{
-    int direction;
-    int write;
+//All possible instructions for a card
+uint8_t head_zero[4] =  {0b000, 0b001, 0b010, 0b011};
+uint8_t head_one[4] =  {0b100, 0b101, 0b110, 0b111};
+
+//Tape that we reuse
+std::vector<int> tape(2048, 0);
+
+/*class TouringMachine{
+    private:
+        //The first card is always the start state
+        //The last card is always the end state
+        uint16_t _cards[13];
+        vector<int> &_stripe;
+        
+    public:
+        TouringMachine(*stripe, uint16_t cards[13]){
+            _stripe = stripe;
+            _cards = cards;
+        }
+    
+}*/
+
+
+std::vector<int< genarateInstructionSet(int amount_of_cards){
+    std::vector<int> cards(amount_of_cards);
     int state;
-};
+
+    for(auto i = 0; i<amount_of_cards; i++){
+        state = state_selector(rand_eng);
+        cards[i] = (head_zero[card_selector(rand_eng)]<<7) | (head_one[card_selector(rand_eng)]<<4) | state;
+    }
+
+    
+    return cards;
+}
 
 int main(){
+ 
     std::cout << "Blub" << std::endl;
     return 0;
 }
